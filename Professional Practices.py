07f3a4057c66be731de1eb272531aed044cc6ab7
mhusnainshafiq12 <mhusnainshafiq4@gmail.com>
2026-06-15 23:11:38 -0700

    Assignment today

---
+++ Professional Practices.py
@@ -0,0 +1,8 @@
+ChoclateName="DairyMilk"
+print(ChoclateName)
+ChoclatePrice=100
+print(ChoclatePrice)
+ChoclateFlavour="Almond"
+print(ChoclateFlavour)
+BuyerName=input("Enter your name ")
+print("code ends here")
