454c841808dd0c7cc8d2bfabbbec075708a647cf
mhusnainshafiq12 <mhusnainshafiq4@gmail.com>
2026-06-24 06:35:49 -0700

    Data Structure tuple code

---
+++ Practice Data structure Tuple.py
@@ -0,0 +1,12 @@
+book_Tuple=(234,"PPSC","Ali Raza",45.9)
+print(book_Tuple)
+print(type(book_Tuple))
+print(len(book_Tuple))
+
+for x in book_Tuple:
+    print(x)
+print(book_Tuple[2])
+print(type(book_Tuple[2]))
+print(book_Tuple[3])
+print(type(book_Tuple[3]))
+
