d2949374806acb9bf6a1bab27e2e544a1b19af77
Muhammad Husnain Shafiq <mhusnainshafiq4@gmail.com>
2026-09-01 06:43:37 -0700

    Delete practice for 15 june.py

--- practice for 15 june.py
+++
@@ -1,9 +0,0 @@
-BookId=7786
-print(BookId)
-BookTitle="Think and Grow Rich"
-print(BookTitle)
-BookPrice=45.5
-print(BookPrice)
-print("My code end here")
-MyName = input("please enter your name")
-print(MyName)
