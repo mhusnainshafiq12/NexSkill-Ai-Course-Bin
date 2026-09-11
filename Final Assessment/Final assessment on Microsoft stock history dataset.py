2207329eab7a9d99661dc0489c3c4b78fb5805e9
Muhammad Husnain Shafiq <mhusnainshafiq4@gmail.com>
2026-09-11 04:01:56 -0700

    Final Assessment — Full Stack AI Course | Batch 11

--- Final Assessment/Final assessment on Microsoft stock history dataset.py
+++ Final Assessment/Final assessment on Microsoft stock history dataset.py
@@ -181,4 +181,4 @@ print("Average Prediction is :",Average_prediction)
 Change=((Average_prediction-Current_Price)/Current_Price)*100
 print("Expected Change is :",round(Change,2),"%")
 
-# According to my prediction this market is best for investment. 
+# According to my prediction this market is best for investment . 
